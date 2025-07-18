55e7947ba94789112653a3d9e6645c9e8cac5d2d
runtime-terror-63 <sayandutta84314@gmail.com>
2025-07-18 23:30:40 +0530

    updated store link generations

--- controllers/storeController.js
+++ controllers/storeController.js
@@ -1,20 +1,22 @@
 const Store = require("../models/Store");
 const User = require("../models/User");
-
+const generateUniqueSlug = require("../utils/generateSlug");
 
 exports.createStore = async (req, res) => {
   try {
-    const { name, address, link } = req.body;
-    const {mobile} = req.query;
+    const { name, address } = req.body;
+    const { mobile } = req.query;
 
-    const user = await User.findOne({mobile});
+    const user = await User.findOne({ mobile });
     const userId = user.id;
+    const slug = await generateUniqueSlug(name);
+    const link = `${process.env.STORE_URL}${slug}`;
     const newStore = new Store({
       name,
       address,
       link,
       userId,
-      mobile:user.mobile
+      mobile: user.mobile,
     });
 
     const savedStore = await newStore.save();
@@ -22,23 +24,24 @@ exports.createStore = async (req, res) => {
     res.status(201).json({
       success: true,
       message: "Store created successfully",
-      store: savedStore
+      storeId: savedStore._id,
+      storeLink: savedStore.link
     });
   } catch (err) {
     res.status(500).json({
       error: "Internal Server Error",
-      details: err.message
+      details: err.message,
     });
   }
 };
 
-
-exports.getMyStore = async (req, res)=>{
-  try{
-    const store = await Store.findOne({userId: req.userId})
-    if(!store) return res.status(404).json({message: "Store not found"})
+exports.getMyStore = async (req, res) => {
+  const { mobile } = req.query;
+  try {
+    const store = await Store.findOne({ mobile });
+    if (!store) return res.status(404).json({ message: "Store not found" });
     res.status(200).json(store);
-  }catch(err){
-    res.status(500).json('Internal Server Error', err);
+  } catch (err) {
+    res.status(500).json("Internal Server Error", err);
   }
-}
+};
